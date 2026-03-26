05cb7c1523a0ba964d17649ce38f69afe917f260
Jonas Henrique <jonas.hs@gmail.com>
2026-03-26 23:20:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8349423327-389b8",
+  "appId": "1:523231471956:web:9ebbca7a4f0d794cfd00c1",
+  "apiKey": "AIzaSyAFf4Gcjs1PsEMHUgBHixm3f9Yy3cKCR1w",
+  "authDomain": "studio-8349423327-389b8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "523231471956"
+};
